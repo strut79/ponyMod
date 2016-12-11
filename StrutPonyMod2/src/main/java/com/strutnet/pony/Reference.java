af616f2b3ba23fe197fa7163eebe92ce82beb8b3
Strut <strut79@gmail.com>
2016-12-11 17:52:14 -0600

    new version 1.11

--- StrutPonyMod2/src/main/java/com/strutnet/pony/Reference.java
+++ StrutPonyMod2/src/main/java/com/strutnet/pony/Reference.java
@@ -2,7 +2,7 @@ package com.strutnet.pony;
 
 public class Reference {
 	public static final String MODID = "strutpony";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.11.0";
 	public static final String MOD_NAME = "strutpony";
 	public static final String CLIENT_PROXY_CLASS = "com.strutnet.pony.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.strutnet.pony.proxy.CcommonProxy";
